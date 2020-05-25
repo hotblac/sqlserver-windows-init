d2e2197a2945559857fff1b8428a69aa10d19579
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2020-05-25 17:21:31 +0100

    Initialize database

--- init.d/run-initialization.ps1
+++ init.d/run-initialization.ps1
@@ -1,5 +1,27 @@
 $cmd = $args
-Write-Host "Do stuff"
+
+######################
+# Start initialization
+######################
+Write-Host "Starting SQL Server for initialization"
+start-service MSSQL`$SQLEXPRESS
+
+if($sa_password -ne "_")
+{
+    Write-Verbose "Changing SA login credentials"
+    $sqlcmd = "ALTER LOGIN sa with password='$($env:sa_password)';ALTER LOGIN sa ENABLE;"
+    & sqlcmd -Q $sqlcmd
+}
+
+Write-Host "Running initialization script"
+sqlcmd -S localhost -U sa -P $env:sa_password -d master -i c:\init.d\create-database.sql
+
+Write-Host "SQL Server initialized. Stopping..."
+stop-service MSSQL`$SQLEXPRESS
+####################
+# End initialization
+####################
+
 
 Write-Host "Running command: $($cmd)"
 Invoke-Expression -Command "$cmd"
